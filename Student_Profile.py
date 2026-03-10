970cd8e63880080a247bd4bcc452f4f29896b324
AScanlon426 <as2937@kent.ac.uk>
2026-03-10 14:46:27 +0000

    Name Function

--- Student_Profile.py
+++ Student_Profile.py
@@ -2,9 +2,14 @@ import json
 import statistics
 
 class Student:
+    def __init__(self, full_name):
+       self.full_name = full_name
+       self.username = self._generate_username(full_name)
+       self.subjects = {}
 
 
 def main():
+    name = input("Enter student's full name: ")
 
 if __name__ == "__main__":
     main()
