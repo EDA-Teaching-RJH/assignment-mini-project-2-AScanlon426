3c043126ee080c9ef335bc55c1652a4544aa39d4
AScanlon426 <as2937@kent.ac.uk>
2026-03-25 16:13:39 +0000

    Test Setup

--- Student_Test.py
+++ Student_Test.py
@@ -3,3 +3,9 @@ import json
 import os
 from unittest.mock import patch, mock_open
 from Student_Profile import Student, Scholarship, Classroom
+
+class TestStudentSystem(unittest.TestCase):
+
+    def setup(self):
+        self.student = Student("Aaron Scanlon")
+        self.classroom = Classroom("classroom_test.json")
