d97cf8493a070817e6a6c89001bc82e0e39d68c6
AScanlon426 <as2937@kent.ac.uk>
2026-03-25 16:29:31 +0000

    Username Tests

--- Student_Test.py
+++ Student_Test.py
@@ -9,3 +9,14 @@ class TestStudentSystem(unittest.TestCase):
     def setup(self):
         self.student = Student("Aaron Scanlon")
         self.classroom = Classroom("classroom_test.json")
+
+    def test_username_format(self):
+        self.assertEqual(self.student.username, "ascanlon")
+
+    def test_username_single(self):
+        s = Student("John")
+        self.asserEqual(s.username, "john")
+
+    def test_username_empty(self):
+        s = Student("")
+        self.assertEqual(s.username, "unkown")
